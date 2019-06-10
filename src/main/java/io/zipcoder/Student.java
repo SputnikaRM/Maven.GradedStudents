e56d25aa6d6040bdac373f5ae33a5becad9c58ae
sputnika <sputnika91@gmail.com>
2019-06-10 08:41:38 -0400

    student

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,37 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores = new ArrayList<>();
+
+    public Student () {}
+
+    public Student(String firstName, String lastName, Double [] examScores){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        // examscores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken () {
+        return 0; // change
+    }
 }
